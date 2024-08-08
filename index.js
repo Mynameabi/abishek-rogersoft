40f70912ecdadcb3f6a140462ea2b95a1f9b111f
Mynameabi <122033460+Mynameabi@users.noreply.github.com>
2024-08-08 22:42:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+const r=require ("./abishek-rogersoft");
+console.log(r.abishek_add(12,12));
+console.log(r.abishek_diff(12,12));
+console.log(r.abishek_mul(12,12));
+console.log(r.abishek_div(12,12));
